cb5ba94a8f78a7008dff5bb7c89abbed8d7a9099
San <josefelipeq@live.com>
2018-05-30 23:33:50 -0500

    Fixed prod mongo uri

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,6 @@ module.exports = {
   steamWebAPIKey: process.env.STEAM_API_KEY,
   cookieKey: process.env.COOKIE_KEY,
   realm: process.env.REALM,
-  returnURL: process.env.RETURN_URL
+  returnURL: process.env.RETURN_URL,
+  mongoURI: process.env.MONGO_URI
 };
