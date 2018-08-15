b28d6e739bbfbb8373012f9258bc19a02d8b19b1
Jose Felipe Quiroga <josefelipeq@live.com>
2018-08-15 15:04:32 -0500

    Simplified Footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,60 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="page-footer blue-grey darken-3">
-        <div className="container">
-          <div className="row">
-            <div className="col l6 s12">
-              <h5 className="white-text">About us</h5>
-              <p className="grey-text text-lighten-4">
-                Add, report and track the most toxic Steam users you find on
-                your multiplayer games. Icons made by{' '}
-                <a href="http://www.freepik.com" title="Freepik" id="contrast">
-                  Freepik
-                </a>{' '}
-                from{' '}
-                <a
-                  href="https://www.flaticon.com/"
-                  title="Flaticon"
-                  id="contrast">
-                  www.flaticon.com
-                </a>{' '}
-                is licensed by{' '}
-                <a
-                  id="contrast"
-                  href="http://creativecommons.org/licenses/by/3.0/"
-                  title="Creative Commons BY 3.0"
-                  target="_blank">
-                  CC 3.0 BY
-                </a>
-              </p>
-            </div>
-            <div className="col l4 offset-l2 s12">
-              <h5 className="white-text">Other</h5>
-              <ul>
-                <li>
-                  <Link to="/" className="highlight">
-                    Home
-                  </Link>
-                </li>
-                <li>
-                  <Link to="/leaderboards" className="highlight">
-                    Most Wanted
-                  </Link>
-                </li>
-                <li>
-                  <a className="highlight" href="https://josefelq.github.io/">
-                    Contact
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
         <div className="footer-copyright">
           <div className="container">
             Copyright © 2018 NoTiltZone, All rights reserved.
